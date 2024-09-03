e42636c878cfee1e6bab1eccbf98de2d707b2e80
MonikaFu <monika.furdyna@gmail.com>
2024-09-03 18:12:02 +0200

    Add techmix_sector file with first outline of the code

---
+++ src/js/techmix_sector.js
@@ -0,0 +1,35 @@
+import * as d3 from 'd3';
+
+export class techmix_sector {
+    constructor(container, data) {
+        // Declare the chart dimensions and margins.
+        const width = 928; // TODO: make variable based on container
+        const height = 500; // TODO: make variable based on container
+        const marginTop = 30; // TODO: make variable based on container - step-wise
+        const marginRight = 0; // TODO: make variable based on container - step-wise 
+        const marginBottom = 30; // TODO: make variable based on container - step-wise (will contain a legend)
+        const marginLeft = 40; // TODO: make variable based on container - step-wise
+
+        // Declare the x scale
+
+        // Declare the y scale
+
+        // Create the svg container
+
+        // Add rectangles for each stacked bar
+
+        // Add bars for green technologies
+
+        // Add the x axis (top and bottom) and tick labels
+
+        // Add the y axis and tick labels
+
+        // Add year labels on the right
+
+        // Add the legend holder (bottom margin)
+
+        // Append legend rectangles
+
+        // Append legend labels
+    }
+}

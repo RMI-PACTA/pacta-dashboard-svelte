be9b39569414949bed4af323a6fb842e68a5f73f
Monika Furdyna <monika.furdyna@gmail.com>
2024-09-24 15:50:48 +0200

    Add techmix per sector plot (#38)
    * Add data for the techmix chart
    
    * Add techmix_sector file with first outline of the code
    
    * Add draft techmix plot
    
    * Add legend
    
    * Add legend green bars
    
    * Draw bars for now and future
    
    * Add year labels
    
    * Add hover-overs
    
    * Fix hover-overs for current year
    
    * Remove unused colours automotive and reverse order
    
    * Read parameter values from selectors
    
    * Fix: Read from scenario selector
    
    * Add event listeners
    
    * Connect equity market selector
    
    * Connect benchmark selector to plot
    
    * Style
    
    * Style css
    
    * Make sure all technologies are in subgroups
    
    * Remove TODO
    
    * Clarify comment
    
    * Remove TODO

--- src/routes/sector_view.svelte
+++ src/routes/sector_view.svelte
@@ -1,23 +1,18 @@
 <!-- src/routes/sector_view.svelte -->
 <script>
 	import { onMount } from 'svelte';
+	import techmix_data from '../json/data_techmix_sector.json';
 	import techmix_future_data from '../json/data_techexposure_future.json';
 	import traj_data from '../json/data_trajectory_alignment.json';
 	import emissions_data from '../json/data_emissions.json';
 	import { techexposure_future } from '../js/techexposure_future.js';
+	import { techmix_sector } from '../js/techmix_sector.js';
 	import { trajectory_alignment } from '../js/trajectory_alignment.js';
 	import { time_line } from '../js/time_line.js';
 
 	onMount(() => {
-		function fetchFutureTechmix() {
-			new techexposure_future(
-				document.querySelector('#techmix-plot'),
-				techmix_future_data,
-				undefined,
-				{
-					default_class: 'Corporate Bonds'
-				}
-			);
+		function fetchTechmix() {
+			new techmix_sector(document.querySelector('#techmix-plot'), techmix_data);
 		}
 
 		function fetchTrajectoryAlignmentData() {
@@ -41,25 +36,30 @@
 				document.querySelector('#content-sector-view').classList.toggle('hidden');
 			});
 
+			const sector_selector = document.querySelector('#sector_selector');
+			sector_selector.addEventListener('change', function () {
+				fetchTechmix();
+			});
+
 			const asset_class_selector = document.querySelector('#asset_class_selector');
 			asset_class_selector.addEventListener('change', function () {
 				const selects = document.querySelectorAll(
-					'.techexposure_class_selector, .time_line_class_selector, .trajectory_class_selector'
+					'.time_line_class_selector, .trajectory_class_selector'
 				);
 				selects.forEach((d) => {
 					d.value = this.value;
 					d.dispatchEvent(new Event('change'));
 				});
+				fetchTechmix();
 			});
 			const benchmark_selector = document.querySelector('#benchmark_selector');
 			benchmark_selector.addEventListener('change', function () {
-				const selects = document.querySelectorAll(
-					'.trajectory_alignment_benchmark_selector, .techexposure_benchmark_selector'
-				);
+				const selects = document.querySelectorAll('.trajectory_alignment_benchmark_selector');
 				selects.forEach((d) => {
 					d.value = this.value;
 					d.dispatchEvent(new Event('change'));
 				});
+				fetchTechmix();
 			});
 			const scenario_source_selector = document.querySelector('#scenario_source_selector');
 			scenario_source_selector.addEventListener('change', function () {
@@ -68,6 +68,7 @@
 					d.value = this.value;
 					d.dispatchEvent(new Event('change'));
 				});
+				fetchTechmix();
 			});
 			const scenario_selector = document.querySelector('#scenario_selector');
 			scenario_selector.addEventListener('change', function () {
@@ -76,10 +77,15 @@
 					d.value = this.value;
 					d.dispatchEvent(new Event('change'));
 				});
+				fetchTechmix();
+			});
+			const equity_market_selector = document.querySelector('#equity_market_selector');
+			equity_market_selector.addEventListener('change', function () {
+				fetchTechmix();
 			});
 		}
 
-		fetchFutureTechmix();
+		fetchTechmix();
 		fetchTrajectoryAlignmentData();
 		fetchEmissionsData();
 		addEventListeners();
@@ -108,8 +114,8 @@
 	</div>
 	<div class="buttons-sector-asset-class p-4 bg-purple-300 flex space-x-2 justify-center">
 		<select class="select max-w-48 variant-outline-surface" id="sector_selector_landing">
-			<option value="Automotive">Automotive</option>
 			<option value="Power">Power</option>
+			<option value="Automotive">Automotive</option>
 			<option value="Oil&gas">Oil & gas</option>
 			<option value="Coal">Coal</option>
 			<option value="Steel">Steel</option>
@@ -127,8 +133,8 @@
 <div class="content p-8 bg-amber-300 hidden" id="content-sector-view">
 	<div class="buttons-sector-asset-class p-4 bg-purple-300 flex space-x-2 justify-center">
 		<select class="select max-w-48 variant-outline-surface" id="sector_selector">
-			<option value="Automotive">Automotive</option>
 			<option value="Power">Power</option>
+			<option value="Automotive">Automotive</option>
 			<option value="Oil&gas">Oil & gas</option>
 			<option value="Coal">Coal</option>
 			<option value="Steel">Steel</option>
@@ -192,16 +198,16 @@
 						<div class="techmix-plot" id="techmix-plot"></div>
 					</div>
 					<div class="emission-intensity-plot-box sm:col-span-3 bg-teal-300">
-						<div class="techmix-explanation">
+						<div class="emission-intensity-explanation">
 							<h4 class="h4">Emission intensity for a sector</h4>
-								<p>
-									Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-									tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
-									eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-									takimata sanctus est Lorem ipsum dolor sit amet.
-								</p>
+							<p>
+								Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+								tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+								eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+								takimata sanctus est Lorem ipsum dolor sit amet.
+							</p>
 						</div>
-						<div class="techmix-plot" id="emission-intensity-plot"></div>
+						<div class="emission-intensity-plot" id="emission-intensity-plot"></div>
 					</div>
 				</div>
 			</div>
@@ -211,33 +217,33 @@
 				<label class="label">
 					<span>Scenario source</span>
 					<select class="select variant-outline-surface" id="scenario_source_selector">
-						<option value="GECO2023">GECO2023</option>
 						<option value="WEO2023">WEO2023</option>
+						<option value="GECO2023">GECO2023</option>
 						<option value="ISF2023">ISF2023</option>
 					</select>
 				</label>
 				<label class="label">
 					<span>Scenario</span>
 					<select class="select variant-outline-surface" id="scenario_selector">
-						<option value="GECO2023: 1.5C">GECO2023: 1.5C</option>
-						<option value="GECO2023: NDC-LTS">GECO2023: NDC-LTS</option>
-						<option value="GECO2023: Reference">GECO2023: Reference</option>
-						<option value="WEO2023: APS">WEO2023: APS</option>
-						<option value="WEO2023: NZE: 2050">WEO2023: NZE: 2050</option>
-						<option value="WEO2023: STEPS">WEO2023: STEPS</option>
-						<option value="ISF2023: 1.5°C">ISF2023: 1.5°C</option>
+						<option value="APS">APS</option>
+						<option value="NZE: 2050">NZE: 2050</option>
+						<option value="STEPS">STEPS</option>
+						<option value="1.5C">1.5C</option>
+						<option value="NDC-LTS">NDC-LTS</option>
+						<option value="Reference">Reference</option>
+						<option value="1.5°C">1.5°C</option>
 					</select>
 				</label>
 				<label class="label">
 					<span>Allocation method</span>
-					<select class="select variant-outline-surface" id="asset_class_selector">
+					<select class="select variant-outline-surface" id="allocation_method_selector">
 						<option value="Portfolio weight">Portfolio weight</option>
 						<option value="Ownership weight">Ownership weight</option>
 					</select>
 				</label>
 				<label class="label">
 					<span>Equity market</span>
-					<select class="select variant-outline-surface" id="scenario_source_selector">
+					<select class="select variant-outline-surface" id="equity_market_selector">
 						<option value="Global Market">Global Market</option>
 						<option value="Developed Market">Developed Market</option>
 						<option value="Emerging Market">Emerging Market</option>

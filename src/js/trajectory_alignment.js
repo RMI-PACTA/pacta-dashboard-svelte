217fee7f9701a1cd2bfb649ca0eb30ff79db1fde
MonikaFu <monika.furdyna@gmail.com>
2024-10-04 16:24:09 -0400

    Add one small multiple volume trajectory

--- src/js/trajectory_alignment.js
+++ src/js/trajectory_alignment.js
@@ -6,221 +6,6 @@ import '../css/plot_styles.css';
 export class trajectory_alignment {
 	constructor(container, data) {
 
-		class trajectory_alignment_per_technology {
-			constructor(containerTech, dataTech, selected_benchmark) {
-
-				let containerTechSelected = d3.select(containerTech);
-
-				const svg = containerTechSelected
-					.append('svg')
-					.attr('width', width)
-					.attr('height', height)
-					.attr('viewBox', [0, 0, width, height])
-					.attr('preserveAspectRatio', 'xMinYMin meet')
-					.attr('style', 'max-width: 100%; height: auto;');
-				
-				// Declare the x scale
-				const x = d3
-				.scaleTime()
-				.range([0, width])
-				.clamp(true)
-				.domain(d3.extent(dataTech, (d) => d.date));
-
-				let y = d3
-				.scaleLinear()
-				.range([height, 0])
-				.clamp(true);
-			
-				svg
-				.append('svg:defs')
-				.append('svg:marker')
-				.attr('id', 'arrow')
-				.attr('viewBox', '0 0 10 10')
-				.attr('refX', 5)
-				.attr('refY', 5)
-				.attr('markerWidth', 6)
-				.attr('markerHeight', 6)
-				.attr('orient', 'auto-start-reverse')
-				.append('svg:path')
-				.attr('d', 'M 0 0 L 10 5 L 0 10 z');
-
-				let production_data = dataTech.filter((d) => !d.benchmark && d.scenario == 'production');
-					if (production_data.length == 0) {
-						containerTech.querySelector('svg').innerHTML = '';
-						return;
-					}
-
-				let benchmark_data = dataTech.filter(d => d.portfolio_name_translation == selected_benchmark);
-				if (benchmark_data.length == 0) {
-					console.log(
-						'No benchmark data available for selected combination of benchmark, allocation method, equity market and scenario source. Try changing the selection.'
-					);
-				}
-
-				let areadata = dataTech.filter((d) => !d.benchmark);
-				dataTech = dataTech.filter(
-					(d) => !d.benchmark || d.portfolio_name_translation == selected_benchmark
-				);
-				if (subddataTechata.length == 0) {
-					containerTech.querySelector('svg').innerHTML = '';
-					return;
-				}
-
-				let production_line_grp = svg.append('g').attr('class', 'production_line_grp');
-
-				let xaxis_grp = svg
-					.append('g')
-					.attr('class', 'xaxis')
-					.attr('transform', 'translate(0,' + height + ')');
-				let yaxis_grp = svg.append('g').attr('class', 'yaxis');
-
-				let yaxislabel_grp = svg.append('g').attr('class', 'yaxislabel');
-
-				let area_paths_grp = svg.append('g').attr('class', 'area_paths');
-
-				let benchmark_line_grp = svg.append('g').attr('class', 'benchmark_line_grp');
-
-// 			let unit = d3.map(production_data, (d) => d.unit_translation).keys()[0];
-
-// 			var sumstat = d3
-// 				.nest()
-// 				.key((d) => d.scenario)
-// 				.entries(areadata)
-// 				.sort((d) => d.key == 'production');
-
-// 			let descending = direction(selected_tech_org);
-
-// 			function format_axis(value_var) {
-// 				if (value_var == 'ratio') {
-// 					return d3.format('.0%');
-// 				} else if (value_var == 'prcnt_chg') {
-// 					return d3.format('.0%');
-// 				} else {
-// 					return d3.format('.2s');
-// 				}
-// 			}
-
-// 			let benchmark_extent;
-// 			let scale_factor;
-// 			if (benchmark_data.length > 0) {
-// 				scale_factor = production_data[0].value / benchmark_data[0].value;
-// 				benchmark_extent = d3.extent(benchmark_data, (d) => d[value_var] * scale_factor);
-// 			} else {
-// 				benchmark_extent = null;
-// 			}
-// 			let production_extent = d3.extent(production_data, (d) => d[value_var]);
-// 			let areadata_extent = d3.extent(areadata, (d) => d[value_var]);
-// 			y.domain(d3.extent([benchmark_extent, production_extent, areadata_extent].flat())).nice();
-
-// 			const num_of_years =
-// 				1 + Math.abs(x.domain().reduce((a, b) => a.getFullYear() - b.getFullYear()));
-
-// 			areas
-// 			let area_paths = chart_group.select('g.area_paths');
-// 			area_paths.selectAll('*').remove();
-
-// 			if (descending) {
-// 				sumstat.sort((a, b) =>
-// 					b.values.filter((d) => d.year == end_year)[0].value >
-// 					a.values.filter((d) => d.year == end_year)[0].value
-// 						? 1
-// 						: -1
-// 				);
-// 			} else {
-// 				sumstat.sort((a, b) =>
-// 					b.values.filter((d) => d.year == end_year)[0].value >
-// 					a.values.filter((d) => d.year == end_year)[0].value
-// 						? -1
-// 						: 1
-// 				);
-// 			}
-
-// 			var area = d3
-// 				.area()
-// 				.x((d) => x(d.date))
-// 				.y0(descending ? height : 0)
-// 				.y1((d) => y(d[value_var]));
-// 			area_paths_grp
-// 				.append('rect')
-// 				.attr('class', 'worse')
-// 				.attr('x', 0)
-// 				.attr('y', 0)
-// 				.attr('width', width)
-// 				.attr('height', height)
-// 				.style('fill', color('worse'));
-
-// 			area_paths = area_paths_grp
-// 				.selectAll('.area')
-// 				.data(sumstat.filter((d) => d.key != 'production'));
-
-// 			area_paths
-// 				.enter()
-// 				.append('path')
-// 				.attr('class', (d) => 'area ' + d.key)
-// 				.attr('d', (d) => area(d.values))
-// 				.style('fill', (d) => color(d.key));
-
-// 			axes
-// 			let tick_labels = d3
-// 				.map(subdata, (d) => d.year)
-// 				.keys()
-// 				.slice(0, Math.min(num_of_years, 5) + 1);
-// 			tick_labels[0] = '31-Dec-' + tick_labels[0] + '*';
-// 			xaxis_grp.call(
-// 				d3
-// 					.axisBottom(x)
-// 					.ticks(Math.min(num_of_years, 5))
-// 					.tickFormat((d, i) => tick_labels[i])
-// 			);
-// 			yaxis_grp.call(d3.axisLeft(y).ticks(8).tickFormat(format_axis(value_var)));
-
-// 			y-axis label
-// 			yaxislabel_grp.selectAll('text').remove();
-
-// 			yaxislabel_grp
-// 				.append('text')
-// 				.attr('class', 'yaxislabel')
-// 				.attr('transform', 'rotate(-90)')
-// 				.attr('y', 0 - margin.left)
-// 				.attr('x', 0 - height / 2)
-// 				.attr('dy', '1em')
-// 				.style('text-anchor', 'middle')
-// 				.style('alignment-baseline', 'bottom')
-// 				.text(selected_tech + ytitle + unit);
-
-// 			production line
-// 			let production_line = d3
-// 				.line()
-// 				.x((d) => x(d.date))
-// 				.y((d) => y(+d[value_var]));
-// 			production_line_grp.selectAll('path').remove();
-// 			production_line_grp.selectAll('.dot').remove();
-
-// 			production_line_grp
-// 				.append('path')
-// 				.attr('class', 'production_line')
-// 				.attr('stroke-width', 1.5)
-// 				.attr('fill', 'none')
-// 				.attr('stroke', 'black')
-// 				.attr('d', (d) => production_line(production_data));
-
-// 			let dots_production = production_line_grp
-// 				.selectAll('.dot')
-// 				.data(production_data)
-// 				.enter()
-// 				.append('circle')
-// 				.attr('class', 'dot')
-// 				.attr('r', 2.5)
-// 				.style('stroke', '#000')
-// 				.style('fill', '#000')
-// 				.attr('cx', (d) => x(d.date))
-// 				.attr('cy', (d) => y(+d[value_var]))
-// 				.on('mouseover', mouseover)
-// 				.on('mousemove', mousemove)
-// 				.on('mouseout', mouseout);
-			}
-		}
-
 		let container_div;
 
 		if (typeof container === 'string') {
@@ -241,12 +26,12 @@ export class trajectory_alignment {
 		this.container = d3.select(container_div);
 
 		// Declare the chart dimensions and margins.
-		const width = 1200;
+		const width = 928;
 		const height = 650;
 		const marginTop = 50;
 		const marginRight = 50;
 		const marginBottom = 200;
-		const marginLeft = 200;
+		const marginLeft = 50;
 
 		// Create the svg container
 		const svg = this.container
@@ -273,26 +58,21 @@ export class trajectory_alignment {
 						'Reference',
 						'1.5\xb0C'
 					];
- 			let ytitle = ' production in ',
+ 		let ytitle = 'Production in ',
  			portfolio_label = 'Portfolio',
  			benchmark_label = 'Benchmark',
  			label_dots_legend = { top: 'Yearly production', bottom: 'volume' },
  			footnote = '* start date of the analysis',
  			hoverover_value_label = '(Planned) yearly production: ';
 
-// 		settings
-// 		const margin = { top: 20, right: 180, bottom: 80, left: 70 };
-// 		let width = ttl_width - margin.left - margin.right;
-// 		let height = ttl_height - margin.top - margin.bottom;
-
  		// parse year to date
  		data.forEach((d) => (d.date = d3.timeParse('%Y')(d.year)));
 	
 		// filter data
-		let selected_class = document.querySelector('#asset_class_selector').value,
+		let selected_class = 'Listed Equity', // document.querySelector('#asset_class_selector').value,
 			selected_sector = document.querySelector('#sector_selector').value,
 			selected_source = document.querySelector('#scenario_source_selector').value,
-			selected_benchmark = document.querySelector('#benchmark_selector').value,
+			selected_benchmark = 'iShares Core S&P 500 ETF',// document.querySelector('#benchmark_selector').value,
 			selected_allocation = document.querySelector('#allocation_method_selector').value,
  			selected_market = document.querySelector('#equity_market_selector').value;
 
@@ -300,7 +80,7 @@ export class trajectory_alignment {
 		.filter((d) => d.asset_class == selected_class)
 		.filter((d) => d.ald_sector == selected_sector)
 		.filter((d) => d.scenario_source == selected_source)
-		.filter((d) => d.this_portfolio | (d.portfolio_name == selected_benchmark))
+		.filter((d) => !d.benchmark | (d.portfolio_name == selected_benchmark))
 		.filter((d) => d.allocation_translation == selected_allocation)
 		.filter((d) => d.equity_market_translation == selected_market)
 
@@ -329,24 +109,245 @@ export class trajectory_alignment {
 		let legend_order = getLegendOrder(selected_source);
 		let color = getColourScale(selected_source);
 
-		let technologyPlotsGroupDiv = document.createElement('div');
-		technologyPlotsGroupDiv.classList = 'volume-trajectory-plots grid sm:grid-cols-12 p-4';
-		document.querySelector('svg').appendChild(technologyPlotsGroupDiv);
-		d3.select(technologyPlotsGroupDiv)
-					.append('g')
-					.attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
+		// Miniature plot dimensions
+		let nrMiniPlots = 3;
+		let paddingBetweenPlots = 50,
+		miniPlotWidth = (width - marginLeft - marginRight - (nrMiniPlots - 1) * paddingBetweenPlots) / nrMiniPlots,
+		miniPlotHeight = (height - marginTop - marginBottom - paddingBetweenPlots) / 2;
+
+		// Declare the x scale
+		const x = d3
+		.scaleTime()
+		.range([0, miniPlotWidth])
+		.clamp(true)
+		.domain(d3.extent(subdata, (d) => d.date));
+
+		let y = d3
+		.scaleLinear()
+		.range([miniPlotHeight, 0])
+		.clamp(true);
 
 		let technology = 'NuclearCap';
-		let technologyDiv = document.createElement('div');
-		technologyDiv.id = 'subchartDiv1';
-		technologyDiv.classList = 'sm:col-span-4';
-		technologyPlotsGroupDiv.appendChild(technologyDiv);
-
 		let subdataTech = subdata.filter(d => d.technology == technology);
 
-		new trajectory_alignment_per_technology(document.querySelector('#subchartDiv1'), subdataTech, selected_benchmark);
+		let i = 0;
+		const group = svg
+			.append('g')
+			.attr('transform', `translate(${(i % 3) * miniPlotWidth + paddingBetweenPlots * Math.max(0, ((i % 3) - 1)) + marginLeft},${Math.floor(i / 3) * miniPlotHeight + paddingBetweenPlots * Math.max(0, (Math.floor(i / 3)) - 1) + marginTop})`);
 
+		let production_data = subdataTech.filter((d) => !d.benchmark && d.scenario == 'production');
+			if (production_data.length == 0) {
+				return;
+			}
+
+		let benchmark_data = subdataTech.filter(d => d.portfolio_name_translation == selected_benchmark);
+		if (benchmark_data.length == 0) {
+			console.log(
+				'No benchmark data available for selected combination of benchmark, allocation method, equity market and scenario source. Try changing the selection.'
+			);
+		}
+
+		let areadata = subdataTech.filter((d) => !d.benchmark);
+		subdataTech = subdataTech.filter(
+			(d) => !d.benchmark || d.portfolio_name_translation == selected_benchmark
+		);
+		if (subdataTech.length == 0) {
+			return;
+		}
+
+		let sumstat = d3
+		.nest()
+		.key((d) => d.scenario)
+		.entries(areadata)
+		.sort((d) => d.key == 'production');
+
+		let descending = direction(technology);
+
+		let benchmark_extent;
+		let scale_factor;
+		if (benchmark_data.length > 0) {
+			scale_factor = production_data[0].value / benchmark_data[0].value;
+			benchmark_extent = d3.extent(benchmark_data, (d) => d.value * scale_factor);
+		} else {
+			benchmark_extent = null;
+		}
+		let production_extent = d3.extent(production_data, (d) => d.value);
+		let areadata_extent = d3.extent(areadata, (d) => d.value);
+		y.domain(d3.extent([benchmark_extent, production_extent, areadata_extent].flat())).nice();
+
+		const num_of_years =
+			1 + Math.abs(x.domain().reduce((a, b) => a.getFullYear() - b.getFullYear()));
+
+		if (descending) {
+			sumstat.sort((a, b) =>
+				b.values.filter((d) => d.year == end_year)[0].value >
+				a.values.filter((d) => d.year == end_year)[0].value
+					? 1
+					: -1
+			);
+		} else {
+			sumstat.sort((a, b) =>
+				b.values.filter((d) => d.year == end_year)[0].value >
+				a.values.filter((d) => d.year == end_year)[0].value
+					? -1
+					: 1
+			);
+		}	
+
+		// Plot areas
+		var area = d3
+			.area()
+			.x((d) => x(d.date))
+			.y0(descending ? miniPlotHeight : 0)
+			.y1((d) => y(d.value));
+
+		let area_paths_grp = group
+			.append('g')
+			.attr('class', 'area_paths');
+
+		let area_paths = group
+			.select('g.area_paths');
+		area_paths.selectAll('*').remove();	
+
+		area_paths_grp
+			.append('rect')
+			.attr('class', 'worse')
+			.attr('x', 0)
+			.attr('y', 0)
+			.attr('width', miniPlotWidth)
+			.attr('height', miniPlotHeight)
+			.style('fill', color('worse'));
+
+		area_paths = area_paths_grp
+			.selectAll('.area')
+			.data(sumstat.filter((d) => d.key != 'production'));
+
+		area_paths
+			.enter()
+			.append('path')
+			.attr('class', (d) => 'area ' + d.key)
+			.attr('d', (d) => area(d.values))
+			.style('fill', (d) => color(d.key));
+
+		// Plot production
+		let production_line_grp = group
+		.append('g')
+		.attr('class', 'production_line_grp');
+
+		let production_line = d3
+			.line()
+				.x((d) => x(d.date))
+				.y((d) => y(+d.value));
+			production_line_grp.selectAll('path').remove();
+			production_line_grp.selectAll('.dot').remove();
+
+			production_line_grp
+				.append('path')
+				.attr('class', 'production_line')
+				.attr('stroke-width', 1.5)
+				.attr('fill', 'none')
+				.attr('stroke', 'black')
+				.attr('d', (d) => production_line(production_data));
+
+			let dots_production = production_line_grp
+				.selectAll('.dot')
+				.data(production_data)
+				.enter()
+				.append('circle')
+				.attr('class', 'dot')
+				.attr('r', 2.5)
+				.style('stroke', '#000')
+				.style('fill', '#000')
+				.attr('cx', (d) => x(d.date))
+				.attr('cy', (d) => y(+d.value))
+				.on('mouseover', mouseover)
+				.on('mousemove', mousemove)
+				.on('mouseout', mouseout);
+
+			let benchmark_line_grp = group.append('g').attr('class', 'benchmark_line_grp');
+
+		// 			benchmark line
+			benchmark_line_grp.selectAll('path').remove();
+			benchmark_line_grp.selectAll('.dot').remove();
+
+			if (benchmark_data.length > 0) {
+				let benchmark_line = d3
+					.line()
+					.x((d) => x(d.date))
+					.y((d) => y(+d.value * scale_factor));
+				benchmark_line_grp
+					.append('path')
+					.attr('class', 'benchmark_line')
+					.attr('stroke-width', 1.5)
+					.attr('fill', 'none')
+					.attr('stroke', 'black')
+					.style('stroke-dasharray', '2,2')
+					.attr('d', (d) => benchmark_line(benchmark_data));
+
+				let dots_benchmark = benchmark_line_grp
+					.selectAll('.dot')
+					.data(benchmark_data)
+					.enter()
+					.append('circle')
+					.attr('class', 'dot')
+					.attr('r', 2.5)
+					.style('stroke', '#000')
+					.style('fill', '#000')
+					.attr('cx', (d) => x(d.date))
+					.attr('cy', (d) => y(+d.value * scale_factor));
+			}
+
+		let unit = d3.map(production_data, (d) => d.unit_translation).keys()[0];
+
+		// x axis
+		let tick_labels = d3
+				.map(subdataTech, (d) => d.year)
+				.keys()
+				.slice(0, Math.min(num_of_years, 5) + 1);
+			tick_labels[0] = '31-Dec-' + tick_labels[0] + '*';
+			
+		group
+			.append('g')
+			.attr('class', 'xaxis')
+			.attr('transform', 'translate(0,' + miniPlotHeight + ')')
+			.call(
+				d3
+					.axisBottom(x)
+					.ticks(Math.min(num_of_years, 5))
+					.tickFormat((d, i) => tick_labels[i])
+			);
 		
+		// y-axis
+		group.append('g')
+		.attr('class', 'yaxis')
+		.call(d3.axisLeft(y).ticks(8).tickFormat(d3.format('.2s')));
+
+		//	y-axis label
+		group
+		.append('g')
+		.attr('class', 'yaxislabel')	
+				.append('text')
+				.attr('class', 'yaxislabel')
+				.attr('transform', 'rotate(-90)')
+				.attr('y', 0 - marginLeft)
+				.attr('x', 0 - miniPlotHeight / 2)
+				.attr('dy', '1em')
+				.style('text-anchor', 'middle')
+				.style('alignment-baseline', 'bottom')
+				.text(ytitle + unit);
+
+		// Title
+		group
+		.append('g')
+		.attr('class', 'mini-plot-title')	
+		.append('text')
+		.attr('y', - marginTop / 2 )
+		.attr('x', miniPlotWidth / 2)
+		.attr('dy', '1em')
+		.style('text-anchor', 'middle')
+		.style('alignment-baseline', 'bottom')
+		.text(technology);
+
 // 		let legend_grp = chart_group
 // 			.append('g')
 // 			.attr('transform', 'translate(' + (width + 10) + ',0)')
@@ -502,37 +503,6 @@ export class trajectory_alignment {
 // 				.style('alignment-baseline', 'central')
 // 				.style('font-size', '0.8em')
 // 				.text((d) => d);
-
-// 			benchmark line
-// 			benchmark_line_grp.selectAll('path').remove();
-// 			benchmark_line_grp.selectAll('.dot').remove();
-
-// 			if (benchmark_data.length > 0) {
-// 				let benchmark_line = d3
-// 					.line()
-// 					.x((d) => x(d.date))
-// 					.y((d) => y(+d[value_var] * scale_factor));
-// 				benchmark_line_grp
-// 					.append('path')
-// 					.attr('class', 'benchmark_line')
-// 					.attr('stroke-width', 1.5)
-// 					.attr('fill', 'none')
-// 					.attr('stroke', 'black')
-// 					.style('stroke-dasharray', '2,2')
-// 					.attr('d', (d) => benchmark_line(benchmark_data));
-
-// 				let dots_benchmark = benchmark_line_grp
-// 					.selectAll('.dot')
-// 					.data(benchmark_data)
-// 					.enter()
-// 					.append('circle')
-// 					.attr('class', 'dot')
-// 					.attr('r', 2.5)
-// 					.style('stroke', '#000')
-// 					.style('fill', '#000')
-// 					.attr('cx', (d) => x(d.date))
-// 					.attr('cy', (d) => y(+d[value_var] * scale_factor));
-// 			}
 
 // 			footnote_group
 // 				.selectAll(null)

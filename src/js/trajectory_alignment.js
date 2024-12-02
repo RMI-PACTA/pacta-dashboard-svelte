929ec9ce95e78d85c9f825b838e1f25625db81c7
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-12-02 13:09:45 +0100

    calling `return` and `break` is superfluous

--- src/js/trajectory_alignment.js
+++ src/js/trajectory_alignment.js
@@ -570,49 +570,41 @@ export class trajectory_alignment {
 						.scaleOrdinal()
 						.domain(['production', '1.5C-Unif', 'NDC-LTS', 'CurPol', 'worse'])
 						.range(['black', '#709458', '#8db96e', '#FDF28D', '#e07b73']);
-					break;
 				case 'WEO2021':
 					return d3
 						.scaleOrdinal()
 						.domain(['production', 'NZE_2050', 'SDS', 'APS', 'STEPS', 'worse'])
 						.range(['black', '#9cab7c', '#c3d69b', '#FFFFCC', '#fde291', '#e07b73']);
-					break;
 				case 'GECO2022':
 					return d3
 						.scaleOrdinal()
 						.domain(['production', '1.5C', 'NDC_LTS', 'Reference', 'worse'])
 						.range(['black', '#9cab7c', '#c3d69b', '#FFFFCC', '#e07b73']);
-					break;
 				case 'ISF2021':
 					return d3
 						.scaleOrdinal()
 						.domain(['production', 'NZE', 'worse'])
 						.range(['black', '#9cab7c', '#e07b73']);
-					break;
 				case 'WEO2022':
 					return d3
 						.scaleOrdinal()
 						.domain(['production', 'NZE_2050', 'APS', 'STEPS', 'worse'])
 						.range(['black', '#9cab7c', '#FFFFCC', '#fde291', '#e07b73']);
-					break;
 				case 'GECO2023':
 					return d3
 						.scaleOrdinal()
 						.domain(['production', '1.5C', 'NDC-LTS', 'Reference', 'worse'])
 						.range(['black', '#9cab7c', '#c3d69b', '#FFFFCC', '#e07b73']);
-					break;
 				case 'ISF2023':
 					return d3
 						.scaleOrdinal()
 						.domain(['production', '1.5\xb0C', 'worse'])
 						.range(['black', '#9cab7c', '#e07b73']);
-					break;
 				case 'WEO2023':
 					return d3
 						.scaleOrdinal()
 						.domain(['production', 'NZE_2050', 'APS', 'STEPS', 'worse'])
 						.range(['black', '#9cab7c', '#FFFFCC', '#fde291', '#e07b73']);
-					break;
 				default:
 					return d3
 						.scaleOrdinal()

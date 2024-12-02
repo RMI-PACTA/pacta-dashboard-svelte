4818e91680f6bc3b5516f75d749a2a84f086dc2e
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-12-02 13:14:43 +0100

    identify purposefully unused var

--- src/js/included_table.js
+++ src/js/included_table.js
@@ -47,7 +47,7 @@ export function tabulateIntoIncludedTable(data, id, opts) {
 
 	var rows = tbody.selectAll('tr').data(data).enter().append('tr');
 
-	var cells = rows
+	var _cells = rows // eslint-disable-line
 		.selectAll('td')
 		.data((row) => {
 			return columns.map((column) => {
